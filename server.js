f2a4e26689e9e08afa77198e11e21ec6bd1af52f
rtacuna <rtacuna@uc.cl>
2017-09-10 11:21:55 -0300

    archivo js

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const PORT = 3000;
+
+let visits = [];
+
+const server = http.createServer((req, res) => {
+  if (visits.length > 19){
+    var x = visits.sort((a , b) => a.date - b.date).shift();
+  }
+
+  visits.push({
+    display: IP: ${req.headers['x-forwarded-for'] || req.connection.remoteAddress }, Time: ${new Date()},
+    date: Date.now()
+  });
+
+  res.end(visits.sort((a , b) => b.date - a.date).map(obj => obj.display).join('\n'));
+})
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log(Listening on port ${PORT})
+});
