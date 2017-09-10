f9d8c5d86d15cc4e8e869a262c50fb06c52e7f4e
rtacuna <rtacuna@uc.cl>
2017-09-10 13:30:13 -0300

    funciona

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
     date: Date.now()
   });
 
-  res.end(visits.sort((a , b) => b.date - a.date).map(obj => obj.display, obj.date).join('\n'));
+  res.end(visits.sort((a , b) => b.date - a.date).map(obj => obj.display).join('\n'));
 })
 
 server.listen(PORT, (err) => {
